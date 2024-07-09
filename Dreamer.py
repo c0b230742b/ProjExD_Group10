4e473e4eb14b9ae97a84109d667d03bdcd84ce0a
C0A23120 <C0A23120af@edu.teu.ac.jp>
2024-07-09 16:04:08 +0900

    beam1

--- Dreamer.py
+++ Dreamer.py
@@ -48,6 +48,32 @@ def main():
         tmr += 1        
         clock.tick(200)
 
+class BeamAllen:
+    """
+    アレンが放つビームに関するクラス
+    """
+    def __init__(self, bird:"Bird"):
+        """
+        ビーム画像Surfaceを生成する
+        引数 bird：ビームを放つこうかとん（Birdインスタンス）
+        """
+        self.img = pg.image.load(f"fig/beam.png")
+        self.rct = self.img.get_rect()
+        self.rct.centery = bird.rct.centery  # こうかとんの中心縦座標をビームの中心縦座標に設定
+        self.rct.left = bird.rct.right  # こうかとん右座標をビーム左座標に設定
+        self.vx, self.vy = +5, 0
+
+    def update(self, screen: pg.Surface):
+        """
+        ビームを速度ベクトルself.vx, self.vyに基づき移動させる
+        引数 screen：画面Surface
+        """
+        if check_bound(self.rct) == (True, True):
+            self.rct.move_ip(self.vx, self.vy)
+            screen.blit(self.img, self.rct)    
+
+
+
 
 if __name__ == "__main__":
     pg.init()
